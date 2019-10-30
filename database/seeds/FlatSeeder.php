8a6fb1ca78af670655bd98ac86c03305267a0f29
carmine-nicoletta <51099284+carminenicoletta91@users.noreply.github.com>
2019-10-30 20:51:44 +0100

    db table:flats-details-users

---
+++ database/seeds/FlatSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Flat;
+use App\User;
+class FlatSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      factory(Flat::class, 400)
+            -> make()
+            -> each(function($flat) {
+
+          $user = User::inRandomOrder() -> first();
+          //funzione category = category_id
+          $flat -> user() -> associate($user);
+
+          $flat -> save();
+
+      });
+    }
+}
