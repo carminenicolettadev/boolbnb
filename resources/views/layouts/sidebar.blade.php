e8118aa1edd67e22fd16dba8926a3d15e6f860bc
flavio-bernasconi <info@flaviobernasconi.it>
2019-11-06 12:29:46 +0100

    sidebar + user modification

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -20,14 +20,16 @@
           </li>
         </ul>
         <div class="btn-sidebar">
-          @if(Request::url() === 'profile/*')
-              <a class="btn-add" href="{{ route('addFlat')}}">Add Flat</a><br>
-          @endif
-          @guest
-            @else
+          @if(Request::url() === 'flat/*')
             <a class="btn-update" href="{{ route('editFlat', $singleFlat ->id)}}">Update Flat</a><br>
             <a class="btn-delete" href="{{ route('deleteFlat', $singleFlat ->id)}}">Delete Flat</a><br>
-          @endguest
+          @else
+              <a class="btn-add" href="{{ route('addFlat')}}">Add Flat</a><br>
+              
+              <a class="btn-add" href="{{ route('edit', $user -> id) }}">Edit Profile</a><br>
+              <a class="btn-add" href="{{ route('destroy', $user -> id) }}">Delete Profile</a><br>
+
+          @endif
           <a href="{{ URL::previous() }}" class="mb-5">Back</a>
         </div>
       </div>
