5c9bd15ef087f19e99fd2d1f91aa2d083f5e81da
flavio-bernasconi <info@flaviobernasconi.it>
2019-11-15 17:20:45 +0100

    style with message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 use App\Message;
 use App\Flat;
 use App\Detail;
@@ -33,15 +35,21 @@ class MessageController extends Controller
     }
     public function messageShow($id)
     {
+        $log = Auth::user()->id;
+        if ($id != Auth::user()->id ) {
+          return redirect("showmesg/$log");
+        }
+        $messages = DB::table('flats')
+        ->join('messages','flat_id', '=', 'flats.id')
+        ->where('user_id', '=', $id)
+        ->orderBy('messages.created_at')
+        ->get();
+        $flat=[];
+        foreach ($messages as $message ) {
+          $flat []= Flat::findOrFail($message ->flat_id);
+        }
 
-      $messages = DB::table('flats')
-      ->join('messages','flat_id', '=', 'flats.id')
-      ->where('user_id', '=', $id)
-      ->orderBy('messages.created_at')
-      ->get();
-
-      // dd($name);
-      return view('mymesg', compact('messages'));
+        return view('mymesg', compact('messages','flat'));
     }
 
 
